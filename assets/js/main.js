fae343c7956f2ca9516add8249e795fbc4f57112
Pola <Pola.kinomoto@gmail.com>
2017-05-21 23:08:20 -0400

    completo

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var subs = document.getElementById("suscribirse").addEventListener("click", function usuario(){
+var nombre=document.getElementById("name").value;
+var mail=document.getElementById("mail").value;
+var direccion=document.getElementById("direccion").value;
+
+	if(nombre.length == "" || mail.length == "" || direccion.length ==""){
+	alert("Faltan datos");
+		}else{
+	alert("Muchas gracias por suscribirse")
+		};
+	});
